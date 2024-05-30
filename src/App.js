4ce2fe99124799ba8b8e27d1ebc059fa9378cec9
F4W <nnurbertus@gmail.com>
2024-05-30 10:38:46 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,53 +1,99 @@
-import { getMovieList, searchMovie } from './api';
+import React, { useEffect, useState } from "react";
+import { getMovieList, searchMovie, getMovieDetails } from './api';
 import './App.css';
-import { useEffect, useState } from "react"
+import { Container, Typography, TextField, Grid, Card, CardMedia, CardContent, Dialog, DialogTitle, DialogContent } from '@mui/material';
+import YouTube from 'react-youtube';
 
 const App = () => {
-  const [popularMovies, setPopularMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const ImgUrl = "https://image.tmdb.org/t/p/w500"
+  const ImgUrl = "https://image.tmdb.org/t/p/w500";
 
   useEffect(() => {
     getMovieList().then((result) => {
-      setPopularMovies(result)
-    })
-  }, [])
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const search = async (q) => {
+    setSearchQuery(q);
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
+  const handleMovieClick = async (movieId) => {
+    const movieDetails = await getMovieDetails(movieId);
+    setSelectedMovie(movieDetails);
+  };
+
+  const handleClose = () => {
+    setSelectedMovie(null);
+  };
 
   const PopularMovieList = () => {
-    return popularMovies.map((movie, i) => {
-      return (
-
-          <div className="Movie-wrapper" key={i}>
-            <div className="Movie-title">{movie.title}</div>
-            <img className="movie-image" alt={movie.title} src={`${ImgUrl}/${movie.poster_path}` } />
-            <div className="Movie-date">Tanggal Rilis : {movie.release_date}</div>
-            <div className="Movie-rate">Rating Film : {movie.vote_average}</div>
-          </div>
-      )
-    })
-  }
-
-
-  const search = async(q) => {
-    if(q.length > 3) {
-   const query = await searchMovie(q)
-   setPopularMovies(query.results)
-    }
-  }
-
-
+    return (
+      <Grid container spacing={3}>
+        {popularMovies.map((movie) => (
+          <Grid item xs={12} sm={6} md={4} lg={2} key={movie.id}>
+            <Card onClick={() => handleMovieClick(movie.id)}>
+              <CardMedia
+                component="img"
+                alt={movie.title}
+                height="300"
+                image={`${ImgUrl}/${movie.poster_path}`}
+                style={{ objectFit: 'cover' }}
+              />
+              <CardContent>
+                <Typography variant="h6">{movie.title}</Typography>
+                <Typography variant="body2">Release Date: {movie.release_date}</Typography>
+                <Typography variant="body2">Rating: {movie.vote_average}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Film List App</h1>
-        <input placeholder="Cari Film Favorit mu :)....." className="Movie-search" onChange={({ target }) => search(target.value)} />
-        <div className="Movie-container">
-          <PopularMovieList />
-        </div>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Film List App
+      </Typography>
+      <TextField
+        fullWidth
+        variant="outlined"
+        placeholder="Cari Film Favorit mu :)....."
+        onChange={({ target }) => search(target.value)}
+        value={searchQuery}
+      />
+      <div style={{ marginTop: '20px' }}>
+        <PopularMovieList />
+      </div>
+      {selectedMovie && (
+        <Dialog open={true} onClose={handleClose} fullWidth maxWidth="md">
+          <DialogTitle>{selectedMovie.title}</DialogTitle>
+          <DialogContent>
+            <Typography variant="h6">Release Date: {selectedMovie.release_date}</Typography>
+            <Typography variant="h6">Rating: {selectedMovie.vote_average}</Typography>
+            <Typography variant="body1" gutterBottom>
+              {selectedMovie.overview}
+            </Typography>
+            {selectedMovie.videos.results.length > 0 && (
+              <YouTube
+                videoId={selectedMovie.videos.results[0].key}
+                opts={{ width: '100%', height: '400px' }}
+              />
+            )}
+          </DialogContent>
+        </Dialog>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,17 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = {
-    key : "d6fdfc5335ac0158c18be7bd161bebce"
-    ,base : "https://api.themoviedb.org/3"
-}
-
+  key: "d6fdfc5335ac0158c18be7bd161bebce",
+  base: "https://api.themoviedb.org/3"
+};
 
 export const getMovieList = async () => {
-    const movie = await axios.get(`${api.base}/movie/popular?page=1&api_key=${api.key}`)
-    return movie.data.results
-}
+  const movie = await axios.get(`${api.base}/movie/popular?page=1&api_key=${api.key}`);
+  return movie.data.results;
+};
 
 export const searchMovie = async (q) => {
-    const search = await axios.get(`${api.base}/search/movie?query=${q}&page=1&api_key=${api.key}`)
-    return search.data
-}
+  const search = await axios.get(`${api.base}/search/movie?query=${q}&page=1&api_key=${api.key}`);
+  return search.data;
+};
+
+export const getMovieDetails = async (movieId) => {
+  const movie = await axios.get(`${api.base}/movie/${movieId}?api_key=${api.key}&append_to_response=videos`);
+  return movie.data;
+};
